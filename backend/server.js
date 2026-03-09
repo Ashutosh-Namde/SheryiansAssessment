c1a77790a35f2d9f990305507bda41b5f4c0303b
Ashutosh Namdev <namdevashutosh38@gmail.com>
2026-03-10 04:47:01 +0530

    auth complete

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-const express = require("express")
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is runing ${process.env.PORT}`);
+    
+})
---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,70 @@
+const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+const registerController = async(req,res)=>{
+    try {
+        const {name , email, password} = req.body;
+        console.log(name, email, password);
+
+        if(!name , !email, !password){
+            return res.status(400).json({message:"Fill All Fields"})
+        }
+
+       const userExist = await userModel.findOne({email})
+       if(userExist){
+        return res.status(400).json({message:"user already exist"})
+       }
+
+
+        const hashPassword = await bcrypt.hashPassword(password,10);
+        
+        const user = await userModel.create({
+            name,
+            email,
+            password :hashPassword
+        })
+        await user.save();
+
+        res.status(201).json({message:"User registered successfully",success:true, user})
+
+        
+    } catch (error) {
+        res.status(500).json({message:"error in register"})
+    }
+    
+}
+
+const loginController = async()=>{
+   try {
+    const {email , password} = req.body
+
+    if(!email, !password){
+            return res.status(400).json({message:"Fill All Fields"})
+        }
+
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"Incorrrect email and password" , success:false})
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordMatch){
+            return res.status(400).json({message:"Incorrrect email and password" , success:false})
+
+        }
+
+        const token = jwt.sign({id:user._id},process.env.JWTSECRET,{expiresIn:"1h"});
+
+        res.status(201).json({message:" User login successfully"})
+   } catch (error) {
+    res.status(500).json({message:"error in login"})
+   }
+    
+}
+
+module.exports = {
+    registerController,
+    loginController
+}
